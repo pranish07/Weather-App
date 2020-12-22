6f935e56aa0b1f454c665919256df637aee2f1b6
Pranish Shrestha <pranishshrestha181@gmail.com>
2020-12-22 19:57:23 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Tempapp from "./weatherinfo/weatherHome.js";
+
+function App(){
+    return(
+        <>
+        <div className="App">
+        <Tempapp />
+        </div>
+        </>
+    );
+}
+
+export default App;
